6f55d44fd81df6a493e93b1014ec062b4945bfd1
Bruno santiago <106166445+brunosantiago2007@users.noreply.github.com>
2023-09-22 08:17:14 -0300

    aula 22/09

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,4 +1,37 @@
 alert('banana');
 
-document.queryselector('.tecla_pom');
-document.queryselector('#som_tecla_pom').play();
+function tocaSomPom(){document.queryselector('.tecla_Pom');
+}
+document.queryselector('tecla_Pom').onclick=tocaSomPom;
+
+function tocaSomClap(){document.queryselector('.tecla_Clap');
+}
+document.queryselector('tecla_Clap').onclick=tocaSomClap;
+
+function tocaSomTim(){document.queryselector('.tecla_Tim');
+}
+document.queryselector('tecla_Tim').onclick=tocaSomTim;
+
+function tocaSompuff(){document.queryselector('.tecla_Puff');
+}
+document.queryselector('tecla_Puff').onclick=tocaSomPuff;
+
+function tocaSomSplash(){document.queryselector('.tecla_Splash');
+}
+document.queryselector('tecla_Splash').onclick=tocaSomSplash;
+
+function tocaSomToim(){document.queryselector('.tecla_Toim');
+}
+document.queryselector('tecla_Toim').onclick=tocaSomToim;
+
+function tocaSomPosh(){document.queryselector('.tecla_Posh');
+}
+document.queryselector('tecla_Posh').onclick=tocaSomPosh;
+
+function tocaSomTic(){document.queryselector('.tecla_Tic');
+}
+document.queryselector('tecla_Tic').onclick=tocaSomTic;
+
+function tocaSomTom(){document.queryselector('.tecla_Tom');
+}
+document.queryselector('tecla_Tom').onclick=tocaSomTom,;
